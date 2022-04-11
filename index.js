c5042eec93619c80fb5e838bc887337454c809f6
Sabrinalvs <biwneta@gmail.com>
2022-04-11 19:48:32 -0300

    Finalizando a lógica e começando o layout da página.

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+$("#imagem").hide();
+$("#video").hide();
+
 $("#botao").click(function(event){
     event.preventDefault();
 
     let data = $("#data").val();
     console.log(data);
+    buscarInfo(data);
+});
 
+function buscarInfo(data){
     $.ajax({
-        url: "https://api.nasa.gov/planetary/apod?api_key=WwC2MaabN3vWiPte7QciJCqkcnADzyWwZDxUSgTl",
-        sucess: function (){
-            
-        }
-    })
-})
+        url: `https://api.nasa.gov/planetary/apod?api_key=WwC2MaabN3vWiPte7QciJCqkcnADzyWwZDxUSgTl&date=${data}`,
+        success: function (resposta){
+            if (resposta.media_type == "image"){
+                $("#video").hide();
+                $("#imagem").show();
+                $("#titulo").text(resposta.title);
+                $("#descricao").text(resposta.explanation);
+                $("#imagem").attr("src", resposta.url);
+
+            } else {
+                $("#imagem").hide();
+                $("#video").show();
+                $("#titulo").text(resposta.title);
+                $("#descricao").text(resposta.explanation);
+                $("#video").attr("src", resposta.url);
+            }
+        },
+    });
+}
